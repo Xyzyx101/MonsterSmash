9b2e51be2aafdce6ac728c38a3af4c6e2577a69a
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-04-17 16:20:41 -0400

    Rough touch control work

--- MonsterSmash/js/input.js
+++ MonsterSmash/js/input.js
@@ -44,6 +44,10 @@
         bindKey(KEY.LEFT_ARROW, ACTION.LEFT);
         bindKey(KEY.DOWN_ARROW, ACTION.DOWN);
         bindKey(KEY.RIGHT_ARROW, ACTION.RIGHT);
+
+        if (ms.isTouchEnabled()) {
+            bindTouchControls();
+        }
     }
 
     function getPressed(action) {
@@ -92,6 +96,107 @@
 
     window.addEventListener('keydown', keyDown.bind(this), false);
     window.addEventListener('keyup', keyUp.bind(this), false);
+
+    /* Touchscreen */
+    var attackButton
+        , jumpButton
+        , dPad
+    ;
+
+    function bindTouchControls() {
+        ms.dom.bind("#gameScreen  #touchControls #attackButton", "touchstart", attackStart);
+        ms.dom.bind("#gameScreen  #touchControls #attackButton", "touchend", attackEnd);
+        ms.dom.bind("#gameScreen  #touchControls #jumpButton", "touchstart", jumpStart);
+        ms.dom.bind("#gameScreen  #touchControls #jumpButton", "touchend", jumpEnd);
+        ms.dom.bind("#gameScreen  #touchControls #dPad", "touchmove", updateDPad);
+        ms.dom.bind("#gameScreen  #touchControls #dPad", "touchend", clearDPad);
+        attackButton = ms.dom.$("#gameScreen  #touchControls #attackButton")[0];
+        jumpButton = ms.dom.$("#gameScreen  #touchControls #jumpButton")[0];
+        dPad = ms.dom.$("#gameScreen  #touchControls #dPad")[0];
+    }
+
+    function attackStart() {
+        if (pressable[ACTION.ATTACK]) {
+            keyPress[ACTION.ATTACK] = true;
+            keyHeld[ACTION.ATTACK] = true;
+            pressable[ACTION.ATTACK] = false;
+        }
+        attackButton.style.backgroundPosition = "150px 0px";
+    }
+
+    function attackEnd() {
+        keyPress[ACTION.ATTACK] = false;
+        keyHeld[ACTION.ATTACK] = false;
+        pressable[ACTION.ATTACK] = true;
+        attackButton.style.backgroundPosition = "0px 0px";
+    }
+
+    function jumpStart() {
+        if (pressable[ACTION.JUMP]) {
+            keyPress[ACTION.JUMP] = true;
+            keyHeld[ACTION.JUMP] = true;
+            pressable[ACTION.JUMP] = false;
+        }
+        jumpButton.style.backgroundPosition = "150px 0px";
+    }
+
+    function jumpEnd() {
+        keyPress[ACTION.JUMP] = false;
+        keyHeld[ACTION.JUMP] = false;
+        pressable[ACTION.JUMP] = true;
+        jumpButton.style.backgroundPosition = "0px 0px";
+    }
+
+    var dPadCenterPoint = null;
+    function updateDPad(e) {
+        if (!dPadCenterPoint) {
+            var dPadRect = ms.dom.$("#gameScreen  #touchControls #dPad")[0].getBoundingClientRect();
+            dPadCenterPoint = {};
+            dPadCenterPoint.x = Math.floor(dPadRect.left + dPadRect.width * 0.5);
+            dPadCenterPoint.y = Math.floor(dPadRect.top + dPadRect.height * 0.5);
+        }
+        var xComp = e.touches[0].clientX - dPadCenterPoint.x;
+        var yComp = e.touches[0].clientY - dPadCenterPoint.y;
+        //console.log("x:" + xComp + " y:" + yComp);
+        //console.log(Math.atan2(yComp, xComp));
+        clearDPad();
+        var dir = Math.atan2(yComp, xComp);
+        if (dir > -0.7 && dir < 0.7) {
+            console.log("right");
+            keyPress[ACTION.RIGHT] = true;
+            keyHeld[ACTION.RIGHT] = true;
+            pressable[ACTION.RIGHT] = false;
+            dPad.style.backgroundPosition = "-600px 0px";
+        } else if (dir < -0.7 && dir > -2.12) {
+            console.log("up");
+            keyPress[ACTION.UP] = true;
+            keyHeld[ACTION.UP] = true;
+            pressable[ACTION.UP] = false;
+            dPad.style.backgroundPosition = "-150px 0px";
+        } else if (dir < -2.12 || dir > 2.12) {
+            console.log("left");
+            keyPress[ACTION.LEFT] = true;
+            keyHeld[ACTION.LEFT] = true;
+            pressable[ACTION.LEFT] = false;
+            dPad.style.backgroundPosition = "-450px 0px";
+        } else if (dir < 2.12 && dir > 0.7) {
+            console.log("down");
+            keyPress[ACTION.DOWN] = true;
+            keyHeld[ACTION.DOWN] = true;
+            pressable[ACTION.DOWN] = false;
+            dPad.style.backgroundPosition = "-300px 0px";
+        }
+    }
+
+    function clearDPad() {
+        var directions = [ACTION.UP, ACTION.DOWN, ACTION.LEFT, ACTION.RIGHT];
+        for (var dir = 0; dir < 4; ++dir) {
+            keyPress[directions[dir]] = false;
+            keyHeld[directions[dir]] = false;
+            pressable[directions[dir]] = true;
+            dPad.style.backgroundPosition = "0px 0px";
+        }
+    }
 
     return {
         getHeld: getHeld
---
+++ MonsterSmash/js/levels/level01.js
@@ -0,0 +1,57 @@
+﻿ms.levels.level00 = {
+    levelSize: {
+        width: 2048
+        ,height: 768
+    }
+    , playerSpawn: {
+        x: 100
+        , y: 100
+    }
+    , background: {
+        image: "images/backgrounds/Background01-half.png"
+        , color: { r: 0, g: 0, b: 0 }
+    }
+    , resources: {
+        audio: ["collapse", "eat", "gun", "machineGun", "powerUp", "roar01", "punch"]
+        , images: [""]
+        , sprites: ["monsterSprite", "PoliceCarSprite", "ProjectileSprite", "PolicemanSprite"]
+    }
+    , buildings: [
+        {
+            buildingStyle: 0
+            , xPos: 200
+            , size: { width: 6, height: 5 }
+        }, {
+            buildingStyle: 3
+            , xPos: 800
+            , size: { width: 3, height: 9 }
+        }, {
+            buildingStyle: 2
+            , xPos: 1150
+            , size: { width: 3, height: 2 }
+        }, {
+            buildingStyle: 2
+            , xPos: 1350
+            , size: { width: 3, height: 2 }
+        }, {
+            buildingStyle: 5
+            , xPos: 1700
+            , size: { width: 5, height: 4 }
+        }
+    ]
+    , spawners: [
+        {
+            type: "PoliceCar"
+            , position: { x: 400 }
+            , rate: 30000
+        }, {
+            type: "PoliceCar"
+            , position: { x: 850 }
+            , rate: 40000
+        }, {
+            type: "PoliceCar"
+            , position: { x: 1700 }
+            , rate: 30000
+        }
+    ]
+};
